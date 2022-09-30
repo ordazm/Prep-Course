ed3b5424f1988e2872141220cc7809603ef5b7cd
ordazm <ordazing@gmail.com>
2022-09-30 19:01:13 -0300

    Home JSI finished

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -230,7 +230,7 @@ function esVocal(letra) {
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
-  //Escribe tu código aquí
+  //Escribe tu código aquí terminado
   var vocal = ["a", "e", "i", "o", "u"];
   if (
     letra == vocal[0] ||
